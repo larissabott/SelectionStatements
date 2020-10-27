53e904df819c2a3cad6dee9cf1e4f1f3ad45a188
[larissa bottefield] <[larissabott3@gmail.com]>
2020-10-26 20:27:40 -0500

    number challenge

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 7;
+
+            Console.WriteLine("Guess the favorite number. Hint: below 10");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("too low! try again");
+            }
+            else if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("sorry, too high! try again");
+            }
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("yay thats it!");
+            }
+            
+        }
+    }
+}
